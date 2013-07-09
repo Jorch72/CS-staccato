2740c1f9012eb0eb80b151eb95dec2b5af7c8593
Drew DeVault <sir@cmpwn.com>
2013-07-08 22:46:01 -0600

    Rememeber desired volume, closes #6

--- staccato/Static/js/home.js
+++ staccato/Static/js/home.js
@@ -184,6 +184,14 @@ $(function() {
     player.addEventListener('ended', function() {
         viewModel.prepareNext();
     });
+    player.addEventListener('volumechange', function() {
+        console.log(player.volume);
+        createCookie('volume', player.volume);
+    });
+    var desiredVolume = readCookie('volume');
+    if (desiredVolume != null) {
+        player.volume = desiredVolume;
+    }
     document.body.addEventListener('keypress', function(e) {
         if (document.activeElement.tagName == 'INPUT')
             return;
@@ -206,4 +214,29 @@ function changeStream(source, start) {
     player.load();
     if (start)
         player.play();
+}
+
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
 }
